2c3efa91ebf7243d56bb85b7aa19836bae9094ab
mzwandile <stuurmanmzwandile@gmail.com>
2021-07-20 11:53:25 +0200

    Initialize Git repository

--- app.py
+++ app.py
@@ -11,8 +11,14 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
 
-    return "<strong>Random joke from chuck norris: </strong>" + response['value']
+    return  response
 
+@app.route('/image',methods=['GET','POST'])
+def get_chuck_image():
+    api_url = 'https://api.chucknorris.io/jokes/random'
+    response = requests.get(api_url).json()
+
+    return response['icon_url']
 
 if __name__ == '__main__':
     app.run(debug=True)
